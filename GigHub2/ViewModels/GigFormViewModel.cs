eea8eb3dd987a68d4f61b6747ee6579474a2ca35
Jerepain <jeremy.thiriet@gmail.com>
2017-03-08 21:46:52 +0100

    create a Gig form

---
+++ GigHub2/ViewModels/GigFormViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using GigHub2.Models;
+
+namespace GigHub2.ViewModels
+{
+    public class GigFormViewModel
+    {
+        public string Venue { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public int Genre { get; set; }
+        public IEnumerable<Genre> Genres { get; set; }
+    }
+}
